fd5b9859a1d0f60ae8bb559d06f79223f0243360
MatthieuLemoine <ml.94230@gmail.com>
2015-06-21 15:44:11 +0200

    Add multiselect support for linux

--- Linux/CreateFolderAndMove.sh
+++ Linux/CreateFolderAndMove.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-fullfile=$NAUTILUS_SCRIPT_SELECTED_URIS
-filename=$(basename "$fullfile")
-extension="${filename##*.}"
-filename="${filename%.*}"
-mkdir $filename 
-mv $filename.$extension $filename/
+IFS='
+'
+printf %s "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" |
+while read -r fullfile
+do
+	filename=$(basename "$fullfile")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+	mkdir $filename 
+	mv $filename.$extension $filename/
+done
